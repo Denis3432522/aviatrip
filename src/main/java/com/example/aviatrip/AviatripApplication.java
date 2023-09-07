e8e4541dd5db62fd0613df0d80c0fa26ab6dbd4d
Denis3432522 <denis_lukyanov_03@mail.ru>
2023-09-07 15:40:11 +1000

    0.0.1 - SNAPSHOT

---
+++ src/main/java/com/example/aviatrip/AviatripApplication.java
@@ -0,0 +1,13 @@
+package com.example.aviatrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviatripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviatripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aviatrip/AviatripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aviatrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviatripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
